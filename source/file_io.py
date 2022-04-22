96ed055b24a3ec5c3f6f0b8e0b56ec39d04f713a
Szymon Świędrych <szymek_sw@outlook.com>
2022-04-22 15:02:27 +0200

    Use with in file operations

--- source/file_io.py
+++ source/file_io.py
@@ -24,21 +24,19 @@ def file_to_node(path: str) -> node.Node:
 
 
 def result_to_file(path: str, solution: [node.Operator]) -> None:
-    file = open(path, "w+")
-    solution_string = ""
+    with open(path, "w+") as file:
+        solution_string = ""
 
-    for operator in solution:
-        solution_string += str(operator)
+        for operator in solution:
+            solution_string += str(operator)
 
-    file.write(str(len(solution)) + '\n')
-    file.write(solution_string)
-    file.close()
+        file.write(str(len(solution)) + '\n')
+        file.write(solution_string)
 
 
 def error_to_file(path: str):
-    file = open(path, "w+")
-    file.write(str(-1))
-    file.close()
+    with open(path, "w+") as file:
+        file.write(str(-1))
 
 
 def stats_to_file(path: str, solution_length: int,
@@ -46,10 +44,9 @@ def stats_to_file(path: str, solution_length: int,
                   number_of_processed_states: int,
                   recursion_depth: int,
                   runtime_duration: float):
-    file = open(path, "w+")
-    file.write(str(solution_length) + '\n')
-    file.write(str(number_of_visited_states) + '\n')
-    file.write(str(number_of_processed_states) + '\n')
-    file.write(str(recursion_depth) + '\n')
-    file.write(str(round(runtime_duration, 3)))
-    file.close()
+    with open(path, "w+") as file:
+        file.write(str(solution_length) + '\n')
+        file.write(str(number_of_visited_states) + '\n')
+        file.write(str(number_of_processed_states) + '\n')
+        file.write(str(recursion_depth) + '\n')
+        file.write(str(round(runtime_duration, 3)))
